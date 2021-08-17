1708ac1b50d5bdbf3d3e585d6465907d319a285a
Willian-AUK <willian.adriano@academico.ifpb.edu.br>
2021-08-17 19:44:44 -0300

    Configurações iniciais.

---
+++ app/src/main/java/com/primeiro/desafio_fuze/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.primeiro.desafio_fuze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
